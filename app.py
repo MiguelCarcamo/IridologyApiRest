a89fda67f9531853f53915845b3be0bee3abbd7b
Miguel Carcamo <carcamomiguelt@gmail.com>
2022-08-10 16:13:55 -0600

    TEST

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
+app.register_blueprint(User.main, url_prefix='/api/user')
+app.register_blueprint(SetupSystems.main, url_prefix='/api/SetupSystems')
+app.register_blueprint(SetupBodyOrgans.main, url_prefix='/api/SetupBodyOrgans')
+app.register_blueprint(SetupFindings.main, url_prefix='/api/SetupFindings')
+app.register_blueprint(SetupSymptoms.main, url_prefix='/api/SetupSymptoms')
 if __name__ == '__main__':
-    app.register_blueprint(User.main, url_prefix='/api/user')
-    app.register_blueprint(SetupSystems.main, url_prefix='/api/SetupSystems')
-    app.register_blueprint(SetupBodyOrgans.main, url_prefix='/api/SetupBodyOrgans')
-    app.register_blueprint(SetupFindings.main, url_prefix='/api/SetupFindings')
-    app.register_blueprint(SetupSymptoms.main, url_prefix='/api/SetupSymptoms')
     app.run(threaded=True, port=5000)
