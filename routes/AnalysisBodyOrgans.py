baacc24f2d7a4a0131c8f152621a1f5203b17d3e
Miguel Carcamo <carcamomiguelt@gmail.com>
2022-10-20 15:01:12 -0600

    Actualizacion para cambiar el lenguaje del informe

--- routes/AnalysisBodyOrgans.py
+++ routes/AnalysisBodyOrgans.py
@@ -3,13 +3,37 @@ from flask import Blueprint, jsonify, request
 from models.AnalysisBodyOrgansModel import AnalysisBodyOrgansModel
 from database.db import get_connection
 
+import pandas as pd
+import json
+from pandas import json_normalize
+
+from googletrans import Translator
+translator = Translator()
+
 main=Blueprint('AnalysisBodyOrgans_blueprint', __name__)
 
 @main.route('/<id>')
 def get_AnalysisBodyOrgans(id):
     try:
         Analysis = AnalysisBodyOrgansModel.get_AnalysisBodyOrgansModel(id)
-        return jsonify(Analysis)
+        df = json_normalize(Analysis)
+        return df.to_json(orient="records")
+    except Exception as ex:
+        return jsonify({'message': str(ex)}), 500
+
+@main.route('/<id>/<lng>')
+def get_AnalysisBodyOrgansLng(id, lng):
+    try:
+        Analysis = AnalysisBodyOrgansModel.get_AnalysisBodyOrgansModel(id)
+        df = json_normalize(Analysis)
+        for m in range(len(df.index)):
+            df.iloc[m,2] = translator.translate(df.iloc[m,2], dest=lng).text
+            df.iloc[m,3] = translator.translate(df.iloc[m,3], dest=lng).text
+            df.iloc[m,5] = translator.translate(df.iloc[m,5], dest=lng).text
+            df.iloc[m,6] = translator.translate(df.iloc[m,6], dest=lng).text
+            df.iloc[m,7] = translator.translate(df.iloc[m,7], dest=lng).text
+            df.iloc[m,8] = translator.translate(df.iloc[m,8], dest=lng).text
+        return df.to_json(orient="records")
     except Exception as ex:
         return jsonify({'message': str(ex)}), 500
 
