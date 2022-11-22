31327cf1a04ece9511b2247bc3fbd7d55998a743
Miguel Carcamo <carcamomiguelt@gmail.com>
2022-11-22 14:20:16 -0600

    Actualizacion base de datos

--- database/db.py
+++ database/db.py
@@ -4,10 +4,10 @@ from psycopg2 import DatabaseError
 def get_connection():
     try:
         return psycopg2.connect(
-            host='ec2-50-19-255-190.compute-1.amazonaws.com',
-            user='waurhzodsccaoj',
-            password='07586a7a5e0edf933eadf34d4fa15a7fef79d8f406ea1bb0c79cc1ee004f029f',
-            database='dcaqg5enote566'
+            host='208.109.191.54',
+            user='postgres',
+            password='Idialogi123',
+            database='iridology'
         )
     except DatabaseError as ex:
         raise ex
